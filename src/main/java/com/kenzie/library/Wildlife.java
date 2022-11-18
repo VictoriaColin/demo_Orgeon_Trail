5693e7863a51716144474ed900a90e3054195c50
Victoria Colin <victoria.colin@mykenzie.snhu.edu>
2022-11-18 13:18:40 -0600

    fixed issues causing gradle tests to fail

--- src/main/java/com/kenzie/library/Wildlife.java
+++ src/main/java/com/kenzie/library/Wildlife.java
@@ -37,27 +37,27 @@ public class Wildlife {
         getDayRoll();
 
         switch (roll) {
-            case 1 -> {
+            case 1: {
                 System.out.println("There's only one rat for today's hunt.");
                 Main.huntOfTheDay = WildlifeOfTheDay.RAT.getFoodQuantity();
             }
-            case 2 -> {
+            case 2: {
                 System.out.println("Caught a rabbit today.");
                 Main.huntOfTheDay = WildlifeOfTheDay.RABBIT.getFoodQuantity();
             }
-            case 3 -> {
+            case 3: {
                 System.out.println("Hunted a deer for dinner.");
                 Main.huntOfTheDay = WildlifeOfTheDay.DEER.getFoodQuantity();
             }
-            case 4 -> {
+            case 4: {
                 System.out.println("Got an elk today.");
                 Main.huntOfTheDay = WildlifeOfTheDay.ELK.getFoodQuantity();
             }
-            case 5 -> {
+            case 5: {
                 System.out.println("Needed help getting the bison. We'll eat well tonight.");
                 Main.huntOfTheDay = WildlifeOfTheDay.BISON.getFoodQuantity();
             }
-            case 6 -> {
+            case 6: {
                 System.out.println("Can't believe we got a bear today. We'll have full tummies 'til the next moon.");
                 Main.huntOfTheDay = WildlifeOfTheDay.BEAR.getFoodQuantity();
             }
